b3625dd6e8d2b1b18950d8975dde4351f3822bad
nburwell <nburwell@4a5fba65-2d3c-6874-b5dc-65002c45b50a>
2010-12-20 05:25:23 +0000

    Added init file to setup lightbox on images

---
+++ lightbox-init.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+$('a[href$=jpg]:has(img)').lightBox();
+$('a[href$=JPG]:has(img)').lightBox();
+$('a[href$=png]:has(img)').lightBox();
+$('a[href$=PNG]:has(img)').lightBox();
+$('a[href$=gif]:has(img)').lightBox();
+$('a[href$=GIF]:has(img)').lightBox();
+});
