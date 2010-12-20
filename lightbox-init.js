2164a8fe409386c4266f1114932b5b10056e995f
nburwell <nburwell@4a5fba65-2d3c-6874-b5dc-65002c45b50a>
2010-12-20 06:30:17 +0000

    Better order on init

--- lightbox-init.js
+++ lightbox-init.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-$('a[href$=jpg]:has(img)').lightBox();
-$('a[href$=JPG]:has(img)').lightBox();
+$('a[href$=jpg|JPG]:has(img)').lightBox();
 $('a[href$=png]:has(img)').lightBox();
 $('a[href$=PNG]:has(img)').lightBox();
 $('a[href$=gif]:has(img)').lightBox();
